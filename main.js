bb27f2d14b1f3d11254dce578eb6a78ab560df4b
MGanom <chewy111@naver.com>
2022-03-04 19:42:44 +0900

    practice start

---
+++ main.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+const app = http.createServer((req, res) => {
+  let url = req.url;
+  if (req.url === "/") {
+    url = "/index.html";
+  }
+  res.writeHead(200);
+  res.end(fs.readFileSync(__dirname + url));
+});
+
+app.listen(3000);
