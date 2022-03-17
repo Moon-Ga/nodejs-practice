d40fe79ac2383fa50e3960ef28f51888609065d4
MGanom <chewy111@naver.com>
2022-03-17 18:06:47 +0900

    add: post update

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ const app = http.createServer((req, res) => {
   const createList = (filelist) => {
     let list = "<ol>";
     for (let i = 0; i < filelist.length; i++) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${i + 1}번째</a></li>`;
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     }
     list = list + "</ol>";
     return list;
   };
 
-  const htmlTemplate = (content) => {
+  const htmlTemplate = (content, update) => {
     return `
     <!DOCTYPE html>
     <html>
@@ -27,8 +27,9 @@ const app = http.createServer((req, res) => {
       </head>
       <body>
         <h1><a href="/">문가네</a></h1>
-        ${createList(dataList)}
+        ${createList(dataList)}<br />
         <a href="/create">글 생성</a>
+        ${update ? update : ""}<br/>
         ${content}
       </body>
     </html>
@@ -40,7 +41,12 @@ const app = http.createServer((req, res) => {
       if (id === null) {
         data = "어서오세요 문가네입니다.";
       }
-      const template = htmlTemplate(data);
+      const update = id
+        ? `
+      <a href="/update?id=${id}">글 수정</a>
+      `
+        : null;
+      const template = htmlTemplate(data, update);
       res.writeHead(200);
       res.end(template);
     });
@@ -56,6 +62,7 @@ const app = http.createServer((req, res) => {
       </p>
     </form>
     `;
+
     const template = htmlTemplate(content);
     res.writeHead(200);
     res.end(template);
@@ -68,6 +75,43 @@ const app = http.createServer((req, res) => {
       const post = new URLSearchParams(body);
       const title = post.get("title");
       const content = post.get("content");
+      fs.writeFileSync(`data/${title}`, content);
+      res.writeHead(303, { Location: encodeURI(`/?id=${title}`) });
+      res.end();
+    });
+  } else if (pathname === "/update") {
+    fs.readFile(`./data/${id}`, "utf-8", (err, data) => {
+      const update = `
+      <a href="/?id=${id}">수정 취소</a>
+      `;
+
+      const content = `
+      <form action="${url.origin}/update_action" method="post">
+        <input type="hidden" name="filename" value="${id}"
+        <p><input type="text" name="title" placeholder="제목" value=${id} /></p>
+        <p>
+          <textarea name="content" placeholder="내용" style="resize:none">${data}</textarea>
+        </p>
+        <p>
+          <input type="submit" />
+        </p>
+      </form>
+      `;
+      const template = htmlTemplate(content, update);
+      res.writeHead(200);
+      res.end(template);
+    });
+  } else if (pathname === "/update_action") {
+    let body = "";
+    req.on("data", (data) => {
+      body = body + data;
+    });
+    req.on("end", () => {
+      const post = new URLSearchParams(body);
+      const filename = post.get("filename");
+      const title = post.get("title");
+      const content = post.get("content");
+      fs.renameSync(`data/${filename}`, `data/${title}`);
       fs.writeFileSync(`data/${title}`, content);
       res.writeHead(303, { Location: encodeURI(`/?id=${title}`) });
       res.end();
